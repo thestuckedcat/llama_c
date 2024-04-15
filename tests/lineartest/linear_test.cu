4aa7f6e8fa1527a6081805a36155b3e99fee617d
StuckedCat <529853411@qq.com>
2024-04-15 23:42:36 +0800

    完成了linear_test

--- tests/lineartest/linear_test.cu
+++ tests/lineartest/linear_test.cu
@@ -4,6 +4,7 @@
 #include<random>
 #include"src/kernels/cublas_utils.h"
 #include"utils/tensor.h"
+#include<algorithm>
 /*
     测试两种矩阵乘法
 
@@ -115,60 +116,146 @@ void CPU_gemm(  const std::vector<std::vector<int>>& shape,
 
 
 
-// 以下的环节必须考虑行主序输入时，使用转置变换为列主序的形式，并确定调用cudakernel时的左右矩阵顺序， 未完成
-void GemmTest_Type1_rowleadingInput(const std::vector<std::vector<int>>& shape,
+// 以下的环节必须考虑行主序输入时，使用转置变换为列主序的形式，并确定调用cudakernel时的左右矩阵顺序
+void GPU_gemm(const std::vector<std::vector<int>>& shape,
                                     std::vector<float*> &data,
-                                    bool trans_a, 
-                                    bool trans_b)
+                                    bool is_row_leading,
+                                    cublasWrapper* cublas_wrapper,
+                                    bool trans1,
+                                    bool trans2)
 {
 
-    // shape:input1, input2, h_output, d_output
+    // data:input1, input2, h_output, d_output
+    // shape: input1, input2, output
+    int input1_size = std::accumulate(shape[0].begin(),shape[0].end,1,std::multiplies<int>());
+    int input2_size = std::accumulate(shape[1].begin(),shape[1].end,1,std::multiplies<int>());
+    int output_size = std::accumulate(shape[3].begin(),shape[3].end,1,std::multiplies<int>());
 
+    // Apply cuda resources
+    float* input1;
+    CHECK(cudaMalloc((void**)&input1, input1_size * sizeof(float)));
+    CHECK(cudaMemcpy(input1, data[0], input1_size * sizeof(float), cudaMemcpyHostToDevice));
+    float* input2;
+    CHECK(cudaMalloc((void**)&input2, input2_size * sizeof(float)));
+    CHECK(cudaMemcpy(input2, data[1], input2_size * sizeof(float), cudaMemcpyHostToDevice));
+    float* output;
+    CHECK(cudaMalloc((void**)&output, output_size * sizeof(float)));
+
+    // Wrap
     DataType float_type = getTensorType<float>();
 
-    TensorWrapper<float> *input1 = new TensorWrapper(   Device::GPU, 
-                                                        DataType::float_type,
-                                                        shape,
-                                                        data[0]);
+    TensorWrapper<float> *left_matrix;
 
-    TensorWrapper<float> *input2 = new TensorWrapper(   Device::GPU,
-                                                        DataType::float_type,
-                                                        shape,
-                                                        data[1]);
+    TensorWrapper<float> *right_matrix;
 
-    TensorWrapper<flaot> *output = new TensorWrapper(   Device::GPU,
-                                                        DataType::float_type,
-                                                        shape,
-                                                        data[3]);
+    TensorWrapper<float> *output_matrix = new TensorWrapper( Device::GPU,
+                                                    float_type,
+                                                    shape[3],
+                                                    output);
 
 
+
+
+    bool trans_a, trans_b, trans_c;
+    if(is_row_leading){
+        // 如果为行主序，需要转成列主序,C = A * B->C^T = B^T * A^T
+        // 两步： 交换左右矩阵顺序，dimension判定是否trans
+        // dimension依据以下顺序判断：是否已经满足矩阵乘要求，是否右矩阵需要转置，是否左矩阵需要转置，是否都需要转置
+        left_matrix = new TensorWrapper(Device::GPU,
+                                        float_type,
+                                        shape[1],
+                                        input2);
+        right_matrix = new TensorWrapper(   Device::GPU,
+                                            float_type,
+                                            shape[0],
+                                            input1);                                       
+
+        // 因为交换了顺序，所以需要多进行一次trans
+        trans_a = !trans2;
+        trans_b = !trans1;
+        trans_c = true;
+        
+    }else{
+        // 列主序，已经满足要求
+        left_matrix = new TensorWrapper(Device::GPU,
+                                        float_type,
+                                        shape[0],
+                                        input1);
+        
+        right_matrix = new TensorWrapper(Device::GPU,
+                                        float_type,
+                                        shape[1],
+                                        input2);
+
+        // 保留trans
+        trans_a = trans1;
+        trans_b = trans2;
+        trans_c = false;
+    }
+
+    // 判断shape是否正确
+    int dim_size = left_matrxi.shape.size();
+    int row_a = !trans_a ? left_matrix.shape[dim_size - 2] : left_matrix.shape[dim_size - 1];
+    int col_a = !trans_a ? left_matrix.shape[dim_size - 1] : left_matrix.shape[dim_size - 2];
+    int row_b = !trans_b ? right_matrix.shape[dim_size - 2] : right_matrix.shape[dim_size - 1];
+    int col_b = !trans_b ? right_matrix.shape[dim_size - 1] : right_matrix.shape[dim_size - 2];
+
+    if(col_a == row_b){
+        //满足要求
+    }else if(row_a == row_b){
+        !trans_a;
+    }else if(col_a == col_b){
+        !trans_b;
+    }else if(row_a == col_b){
+        !trans_a;
+        !trans_b;
+    }
+
+    if(shape[0].size() == 2)
+        launchLinearGemm(   left_matrix,
+                            right_matrix, 
+                            output_matrix,
+                            cublas_wrapper,
+                            trans_a,
+                            trans_b,
+                            trans_c);
+    else if(shape[0].size() > 2)
+        launchLinearStrideBatchGemm(left_matrix,
+                                    right_matrix,
+                                    output_matrix,
+                                    cublas_wrapper,
+                                    trans_a,
+                                    trans_b,
+                                    trans_c);
+
+    CHECK(cudaMemcpy(data[3], output->data, output_size * sizeof(float), MemcpyDeviceToHost));
+
+    cudaFree(input1);
+    cudaFree(input2);
+    cudaFree(output);
+    delete left_matrix;
+    delete right_matrix;
+    delete output_matrix;
 }                                                        
-void GemmTest_Type2_rowleadingInput(std::vector<float*> data,
-                                    const std::vector<int>& shape,                     
-                                    cublasWrapper* cublas_wrapper,
-                                    bool is_row_leading)
+
+bool CheckResult(const std::vector<std::vector<int>>& shape,
+                                    std::vector<float*> &data)
 {
+    //data->[left_matrix, right_matrix, cpu_output_matrix, gpu_output_matrix]，输出矩阵都已归零
+    //shape->input1, input2, output->[dim1,dim2,dim3,dim4]，在这里[dim3,dim4]才是用来计算的矩阵，其他是批次
 
-}
+    int output_size = std::accumulate(shape[3].begin(),shape[3].end(), 1, std::multiplies<int>());
 
-void BatchedGemmTest_Type1( std::vector<float*> data, 
-                            const std::vector<int>& shape,
-                            cublasWrapper* cublas_wrapper,
-                            bool is_row_leading)
-{
-
-}
-
-void BatchedGemmTest_Type2( std::vector<float*> data,
-                            const std::vector<int>& shape,
-                            cublasWrapper* cublas_wrapper,
-                            bool is_row_leading)
-{
-
-}
-
-bool CheckResult(float* CPU_res, float* GPU_res, int nsize)
-{
+    for(int i = 0; i < output_size; i++){
+        if(fabs(data[2][i] - data[3][i]) > 1e-5){
+            std::cout << "Wrong data " << std::endl;
+            std::cout << "cpu data:  " << data[2][i] << std::endl;
+            std::cout << "gpu data:  " << data[3][i] << std::endl;
+            return false;
+        }
+    }
+    std::cout << "All data remain true" << std::endl;
+    return true;
 
 }
 
@@ -178,15 +265,9 @@ std::vector<float*> generate_data(const std::vector<std::vector<int>>& shape){
     // 存入input1,input2,h_output, d_output
     std::vector<float*> res;
 
-    int total_size_input1 = 1;
-    int total_size_input2 = 1;
-    int total_size_output = 1;
-
-    for(int i = 0; i < shape[0].size();i++){
-        total_size_input1 *= shape[0][i];
-        total_size_input2 *= shape[1][i];
-        total_size_output *= shape[2][i];
-    }
+    int total_size_input1 = std::accumulate(shape[0].begin(), shape[0].end(), 1, std::multiplies<int>());
+    int total_size_input2 = std::accumulate(shape[1].begin(), shape[1].end(), 1, std::multiplies<int>());
+    int total_size_output = std::accumulate(shape[2].begin(), shape[2].end(), 1, std::multiplies<int>());
 
     float* input1   =   (float*)malloc(sizeof(float) * total_size_input1);
     float* input2   =   (float*)malloc(sizeof(float) * total_size_input2);
@@ -215,4 +296,13 @@ std::vector<float*> generate_data(const std::vector<std::vector<int>>& shape){
 
 
     return res;
+}
+
+
+int main(){
+    std::vector<std::vector<int>> shape{{}}
+    std::vector<float>* data = generate_data(shape, trans_a, trans_b);
+
+    CPU_gemm(shape, data, trans_a, trans_b);
+    GPU_gemm(shape, data, is_row_leading, cublas_wrapper, trans_a, trans_b);
 }
